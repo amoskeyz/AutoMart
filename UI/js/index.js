9c7f70b2bbe8db69f5e06e491fbdc0c43783b79e
amoskeyz <inspiron.amos@gmail.com>
2019-06-26 15:17:10 +0100

    feat(power ui) Add functionality to the sign up feature >- consume sign up api endpoint [Delivers #166929015]

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-const */
 let barr = document.querySelector('.nav-bar');
 const back = document.querySelector('.forward_button');
 const dialog = document.querySelector('dialog');
@@ -14,133 +17,132 @@ const email = document.querySelector('.name');
 const label = document.querySelector('.label');
 
 
+// console.log(spinner);
+
+
 email.addEventListener('focus', () => {
+  label.style.top = '-40px';
+  label.style.fontSize = '13px';
+});
+
+email.addEventListener('input', (event) => {
+  event.preventDefault();
+  const str = email.value;
+  if (str.length > 0) {
     label.style.top = '-40px';
     label.style.fontSize = '13px';
-  });
-  
-email.addEventListener('input', (event) => {
-    event.preventDefault();
-    const str = email.value;
-    if (str.length > 0) {
-      label.style.top = '-40px';
-      label.style.fontSize = '13px';
-    }
-  });
-  
+  }
+});
+
 email.addEventListener('focusout', () => {
-    const str = email.value;
-    if (str.length < 1) {
-      label.style.top = '-20px';
-      label.style.fontSize = '16px';
-    }
-  });
+  const str = email.value;
+  if (str.length < 1) {
+    label.style.top = '-20px';
+    label.style.fontSize = '16px';
+  }
+});
 
 
 window.addEventListener('scroll', (event) => {
   event.preventDefault();
-const screenScroll = document.documentElement.scrollTop;
-   if (screenScroll > 100 && screenScroll < 550){
-     barr.style.top = '-100px';
-   } else{
+  const screenScroll = document.documentElement.scrollTop;
+  if (screenScroll > 100 && screenScroll < 550) {
+    barr.style.top = '-100px';
+  } else {
     barr.style.top = '0px';
-   }
-	
-	input.checked = false;
-	
-	if(screenScroll < 100) {
-		navbar.classList.remove('whiteBackground');
-		navlink.forEach(nav => { nav.classList.remove('themeColor');
-		});
-		navLogo.classList.remove('darkTheme');
-	}
-	else {
-		navbar.classList.add('whiteBackground');
-		navlink.forEach(nav => { nav.classList.add('themeColor');
-		});
-		navLogo.classList.add('darkTheme');
-	}
+  }
+
+  input.checked = false;
+
+  if (screenScroll < 100) {
+    navbar.classList.remove('whiteBackground');
+    navlink.forEach((nav) => {
+      nav.classList.remove('themeColor');
+    });
+    navLogo.classList.remove('darkTheme');
+  } else {
+    navbar.classList.add('whiteBackground');
+    navlink.forEach((nav) => {
+      nav.classList.add('themeColor');
+    });
+    navLogo.classList.add('darkTheme');
+  }
 });
 
 const delay = (time) => {
-	const newTime = new Promise((resolve, reject) => {
-		setTimeout(() => {
-		  resolve();
-		}, time);
-	})
-	return newTime;
-} 
+  const newTime = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+  return newTime;
+};
 let change = 0;
 const moveLeft = async () => {
-change = 0;
-for (let i = 0; i < article.length -3; i++){
+  change = 0;
+  for (let i = 0; i < article.length - 3; i++) {
     scrollingAds.style.left = `-${change}px`;
-	await delay(5000);
-	change = change + 320;
-}
-	for (let i = 0; i < article.length -2; i++){
+    await delay(5000);
+    change += 320;
+  }
+  for (let i = 0; i < article.length - 2; i++) {
     scrollingAds.style.left = `-${change}px`;
-	await delay(5000);
-	change = change - 320;
-}
-}
+    await delay(5000);
+    change -= 320;
+  }
+};
 
-(async function work(){ await moveLeft();})();
-back.addEventListener('click',(e)=>{
-	if (change !== ((article.length - 3)* 320 )){
-	change = change + 320;
-	scrollingAds.style.left = `-${change}px`;
-}
-	
-})
-forward.addEventListener('click',(e)=>{
-	if(change !== 0){
-		change = change - 320;
-	scrollingAds.style.left = `-${change}px`;
-	}
-})
+(async function work() { await moveLeft(); }());
+back.addEventListener('click', () => {
+  if (change !== ((article.length - 3) * 320)) {
+    change += 320;
+    scrollingAds.style.left = `-${change}px`;
+  }
+});
+forward.addEventListener('click', () => {
+  if (change !== 0) {
+    change -= 320;
+    scrollingAds.style.left = `-${change}px`;
+  }
+});
 
-article.forEach(article =>{
-	article.addEventListener('click',(e)=>{
-	dialog.showModal();
-		dialog.className = 'grow';
-	let initialPath = e.path[0];
-		let namePath = e.path[1];
-		console.log(e.path[1]);
-//		e.path[1].lastChild.previousSibling.innerTex
-//		console.log(e);
-	if(initialPath.tagName !== 'IMG'){
-		initialPath = e.path[0].parentElement.firstElementChild;
-		namePath = e.path[1].lastChild.previousSibling.lastElementChild
-		if(initialPath.tagName !== 'IMG')
-		initialPath = initialPath.firstElementChild;
-		namePath = e.path[1].lastChild.previousSibling.lastElementChild
-}
-
-		
-		showDialog.innerHTML =
-		
-		`<div class = "dialog_all"><div class="dialog_left">
-					 <div class="dialog_logo">AutoMart</div>
-					 <img src = ${initialPath.src}>
-					 </div>
-					<div class="dialog_car">
-						<p>Please Sign-up or Login to Contact Dealers</p>
-						<hr>
-						<h2 class="dialog_carName">2018 Toyota Supra</h2>
-						<p class="l ocation icon">Lagos</p>
-						<p class="dialog_price">N600,000</p>
-						<div class="dialog_button">
-							<button class="dialog_signUp"><a class = "clicker" href="">Sign-Up</a></button>
-							<button class="dialog_login"><a class="clicker" href="">Login</a></button>
-						</div>
-					 </div></div`;
-	})
-})
+article.forEach((art) => {
+  art.addEventListener('click', (e) => {
+    dialog.showModal();
+    dialog.className = 'grow';
+    let initialPath = e.path[0];
+    let namePath = e.path[1];
+    console.log(e.path[1]);
+    // e.path[1].lastChild.previousSibling.innerTex
+    // console.log(e);
+    if (initialPath.tagName !== 'IMG') {
+      initialPath = e.path[0].parentElement.firstElementChild;
+      namePath = e.path[1].lastChild.previousSibling.lastElementChild;
+      if (initialPath.tagName !== 'IMG') { initialPath = initialPath.firstElementChild; }
+      namePath = e.path[1].lastChild.previousSibling.lastElementChild;
+    }
 
 
-close.addEventListener('click',(e)=>{
-	setTimeout( function(){dialog.close();}, 300);
-	dialog.classList.remove('grow');
-})
+    showDialog.innerHTML = `<div class = "dialog_all"><div class="dialog_left">
+<div class="dialog_logo">AutoMart</div>
+<img src = ${initialPath.src}>
+</div>
+<div class="dialog_car">
+<p>Please Sign-up or Login to Contact Dealers</p>
+<hr>
+<h2 class="dialog_carName">2018 Toyota Supra</h2>
+<p class="l ocation icon">Lagos</p>
+<p class="dialog_price">N600,000</p>
+<div class="dialog_button">
+<button class="dialog_signUp"><a class = "clicker" href="">Sign-Up</a></button>
+<button class="dialog_login"><a class="clicker" href="">Login</a></button>
+</div>
+</div></div`;
+  });
+});
 
+
+close.addEventListener('click', () => {
+  setTimeout(() => { dialog.close(); }, 300);
+  dialog.classList.remove('grow');
+});
