6ddac355f71522d04bf29017de83994389e0cde9
amoskeyz <inspiron.amos@gmail.com>
2019-07-02 19:52:35 +0100

    feat(power ui) Add functionality to sign in feature >- consume sign in endpoint [Delivers #167051034]

---
+++ UI/js/config.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const { authtoken } = localStorage;
+
+// const devURL = 'http://localhost:3000/api/v1/';
+const devURL = 'https://andela-automart-amos.herokuapp.com/api/v1/';
+// const userURL = 'http://localhost:5001/api/v2/users/';
+
+const fetcher = async (url, method, body = undefined) => {
+  const object = {
+    method,
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      authtoken,
+    }),
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(url, object);
+  const statusCode = response.status;
+  const responseObj = await response.json();
+  console.log(responseObj);
+  return { responseObj, statusCode };
+};
