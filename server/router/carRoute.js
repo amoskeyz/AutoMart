991f424c1c1b2d84d122782e844798ed83f1d70d
amoskeyz <inspiron.amos@gmail.com>
2019-07-24 16:49:30 +0100

    Bug(Feedback) Implement LFA Feedback >- rename isUser method to isRegularUser [Fixes #167491156]

--- server/router/carRoute.js
+++ server/router/carRoute.js
@@ -4,13 +4,13 @@ import validator from '../middleware/validator';
 import authenticator from '../middleware/authentication';
 
 const carRouter = express();
-carRouter.post('/car/', authenticator.authenticateUser, authenticator.isUser, validator.validateCar, carController.postAds);
-carRouter.patch('/car/:carId/status', authenticator.authenticateUser, authenticator.isUser, validator.validateSold, carController.markSold);
-carRouter.patch('/car/:carId/price', authenticator.authenticateUser, authenticator.isUser, validator.validateUpdateCar, carController.updateCar);
-carRouter.get('/car/:carId/', authenticator.authenticateUser, authenticator.isUser, carController.specificCar);
+carRouter.post('/car/', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateCar, carController.postAds);
+carRouter.patch('/car/:carId/status', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateSold, carController.markSold);
+carRouter.patch('/car/:carId/price', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateUpdateCar, carController.updateCar);
+carRouter.get('/car/:carId/', authenticator.authenticateUser, authenticator.isRegularUser, carController.specificCar);
 carRouter.get('/car', authenticator.authenticateUser, validator.validateGetCar, carController.car);
-carRouter.post('/flag/:carId', authenticator.authenticateUser, authenticator.isUser, validator.validateFlag, carController.flagCar);
-carRouter.patch('/cars/:car_id', authenticator.authenticateUser, authenticator.isUser, carController.updateCarImage);
+carRouter.post('/flag/:carId', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateFlag, carController.flagCar);
+carRouter.patch('/cars/:car_id', authenticator.authenticateUser, authenticator.isRegularUser, carController.updateCarImage);
 
 
 export default carRouter;
