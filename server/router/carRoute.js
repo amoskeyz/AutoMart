8b987481f76568e71e4da57edce3f9bbc3f56624
amoskeyz <inspiron.amos@gmail.com>
2019-10-08 17:29:24 +0100

    update get all car endpoint

--- server/router/carRoute.js
+++ server/router/carRoute.js
@@ -8,7 +8,7 @@ carRouter.post('/car/', authenticator.authenticateUser, authenticator.isRegularU
 carRouter.patch('/car/:carId/status', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateSold, carController.markSold);
 carRouter.patch('/car/:carId/price', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateUpdateCar, carController.updateCar);
 carRouter.get('/car/:carId/', authenticator.authenticateUser, authenticator.isRegularUser, carController.specificCar);
-carRouter.get('/car', authenticator.authenticateUser, validator.validateGetCar, carController.car);
+carRouter.get('/car', validator.validateGetCar, carController.car);
 carRouter.post('/flag/:carId', authenticator.authenticateUser, authenticator.isRegularUser, validator.validateFlag, carController.flagCar);
 carRouter.patch('/cars/:car_id', authenticator.authenticateUser, authenticator.isRegularUser, carController.updateCarImage);
 
